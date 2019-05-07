a4abf04d25e059a5d69d29cecdf5bdd62bd4ba98
Chris Stubbs <ChristopherJStubbs93@gmail.com>
2019-05-07 11:40:47 -0700

    added Palette and css to show

--- src/Palette.js
+++ src/Palette.js
@@ -1,12 +1,17 @@
 import React, { Component } from 'react';
+import Colorbox from './Colorbox';
+import './Palette.css';
 
 class Palette extends Component {
 
     render() {
+        const colorBoxes = this.props.colors.map(color => (
+            <Colorbox background={color.color} name={color.name} />
+        ))
         return (
             <div className="Palette">
                 <div className="Palette-Colors">
-                    
+                    {colorBoxes}
                 </div>
             </div>
         );
