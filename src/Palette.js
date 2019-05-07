791d4792dac3fa097434241dbe0518387049428c
Chris Stubbs <ChristopherJStubbs93@gmail.com>
2019-05-07 11:20:39 -0700

    initial commit

---
+++ src/Palette.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class Palette extends Component {
+
+    render() {
+        return (
+            <div className="Palette">
+                <div className="Palette-Colors">
+                    
+                </div>
+            </div>
+        );
+    }
+
+}
+
+export default Palette;
