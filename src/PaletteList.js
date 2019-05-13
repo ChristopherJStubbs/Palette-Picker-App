d9b4e579a6b3b27ff6f8de50bc277c5a9d906a2d
Chris Stubbs <ChristopherJStubbs93@gmail.com>
2019-05-13 16:28:31 -0700

    fixed problem

---
+++ src/PaletteList.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import {Link} from 'react-router-dom';
+import MiniPalette from './MiniPalette';
+
+class PaletteList extends Component {
+
+    render() {
+        const { palettes } = this.props;
+        return (
+            <div>
+                <MiniPalette />
+                <h1>Color List</h1>
+                {palettes.map(palette => (
+                    <MiniPalette {...palette} />
+                ))}
+            </div>
+        );
+    }
+
+}
+
+export default PaletteList;
