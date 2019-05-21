33a89f87e349cfb7dc5e564827c70147f2a36565
Chris Stubbs <ChristopherJStubbs93@gmail.com>
2019-05-21 12:29:20 -0700

    Added link to create your own palette and started an empty component for it

---
+++ src/NewPaletteForm.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class NewPaletteForm extends Component {
+
+    render() {
+        return (
+            <div>
+                <h1>Create New Palette</h1>
+            </div>
+        );
+    }
+
+}
+
+export default NewPaletteForm;
--- src/PaletteList.js
+++ src/PaletteList.js
@@ -17,6 +17,7 @@ class PaletteList extends Component {
                 <div className={classes.container}>
                     <nav className={classes.nav}>
                         <h1>Perfect Palette</h1>
+                        <Link to='/palette/new'>Create New Palette</Link>
                     </nav>
                     <div className={classes.palettes}>
                         {palettes.map(palette => (
